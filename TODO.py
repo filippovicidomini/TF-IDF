8cfb741696f44311344180353a258ffe3907e4de
Filippo Vicidomini <filippovicidomini@gmail.com>
2022-12-13 21:17:29 +0100

    da fare ancora molte cross validation  - angolare  - knn  - polinomiale  - lineare
    da fare tutto su gaussiano

--- TODO.py
+++ TODO.py
@@ -1,24 +1,2 @@
-import time
-
-import matplotlib.pyplot as plt
-import pandas as pd
-import seaborn as sn
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import confusion_matrix
-from sklearn.model_selection import train_test_split
-from sklearn.svm import SVC
-
-number: list = [1, 10, 20, 50, 100, 150, 200, 250, 300, 400]
-
-for i in range(len(number)):
-    RFC = RandomForestClassifier(n_estimators=number[i])
-
-    start_time = time.time()
-    RFC.fit(X_train, y_train)
-    print('Training time: %f' % (time.time() - start_time))
-    RFC.score(X_test, y_test)
-    start_time = time.time()
-    y_predict = RFC.predict(X_test)
-    print('Prediction time: %f' % (time.time() - start_time))
-    print('Missclassified examples: %d' % (y_test != y_predict).sum())
-    print('Accuracy: %.3f' % accuracy_score(y_test, y_predict))
+# TODO: gaussiano
+# TODO: rifare cross validation per: angolare, gaussiano, linearen e polinomiale,  knn
